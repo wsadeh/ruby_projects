1dce88065cd9d5536b77ee9d834025f0929ff729
wsadeh <wsadeh@perforce.com>
2014-03-10 14:19:12 -0700

    Initial commit

---
+++ first_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '2691189c9b83064d68f25d2df9884d166337ba1862e6262dd1484040bca635e153218507a40c7042089994a0e43aeabbd9c29b18ecd477deec058155d02373ed'
