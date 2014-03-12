1e1a4be6f663679fc544867859e9a2a29f85f4f0
wsadeh <wsadeh@perforce.com>
2014-03-12 15:49:19 -0700

    Initial check-in

---
+++ sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = 'e272a60abdcba291e1634d53b8f80aba3ab46d94d4be48c9f051fa0a93960362460335f334e81249475b8ae6ed2a77a522c7d4673ec2b625336e41da41af337e'
